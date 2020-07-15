a8ee8b3337f214696938517dc591627ec2852765
Kathryn Panger <kpanger88@gmail.com>
2020-07-15 16:32:07 -0700

    testing mongo

--- mars_flask.py
+++ mars_flask.py
@@ -1,18 +1,38 @@
 ############
 #Flask Ap Starts Here
 ############
-from flask import Flask
+from flask import Flask, render_template
+import pymongo
+#from scrape.py import scrape
 
-app=Flask(__name__)
+app = Flask(__name__)
 
+conn='mongodb://localhost:27017'
+client=pymongo.MongoClient(conn)
 
+db=client.mars_db
+mars_info = db.mars
+db.mars.drop()
+
+db.mars.insert_many(
+    [
+        {
+            'player': 'Jessica',
+            'position': 'Point Guard'
+        },
+        {
+            'player': 'Mark',
+            'position': 'Center'
+        }
+    ]
+)
 @app.route("/")
 def home():
-    return("go to scrape")
+    mongolist=list(db.mars.find())
+    print(mongolist)
+    return render_template("index.html",text="Mission to Mars?", data=mongolist)
 
-@app.route("/scrape")
-def scrape():
-    from scrape.py import scrape
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
